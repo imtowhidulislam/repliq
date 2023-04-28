d06b3c5b891d148fee27b6a1190d391e8651820b
Towhid <niamaaktar@gmail.com>
2023-04-28 22:19:07 +0600

    homepage para text problem fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,8 +16,8 @@ const Home = () => {
           <header className='container grid grid-cols-homepageLayoutHero1 place-items-center gap-12 md:gap-16 lg:gap-20'>
             <div>
               <h2 className='capitalize text-4xl sm:text-5xl lg:text-7xl font-bold font-mono text-left text-[#1c1760]'>shopping and department store</h2>
-              <p className='text-sm sm:text-base md:text-sm text-[#fff] py-4 text-left'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores eaque eligendi animi accusamus voluptatibus cupiditate consequuntur doloribus repudiandae adipisci temporibus?</p>
-              <p className='text-sm sm:text-base md:text-sm text-[#ffffff] pb-4 text-left'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit, voluptatem!</p>
+              <p className='text-sm sm:text-base md:text-base text-[#fff] py-4 text-left'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores eaque eligendi animi accusamus voluptatibus cupiditate consequuntur doloribus repudiandae adipisci temporibus?</p>
+              <p className='text-sm sm:text-base md:text-base text-[#ffffff] pb-4 text-left'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit, voluptatem!</p>
               <div className=''>
                 <li className=' flex justify-start w-max bg-green-500 text-gray-100 font-bold cursor-pointer py-3 px-8 rounded-full'>
                   <Link to="/product">
